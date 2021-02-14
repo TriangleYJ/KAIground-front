1ef5fb007f37bf58e7c39e66c2636c5d0f62cc96
Triangleyj <dpwns99@gmail.com>
2021-02-15 02:11:06 +0900

    Add basic components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <Header/>
+    <div class="wrapper">
+      <v-container class="grey lighten-5">
+        <v-row no-gutters dense>
+          <v-col cols="5">
+            <Map/>
+          </v-col>
+          <v-col cols="2">
+            <Stamina/>
+          </v-col>
+          <v-col cols="2">
+            <Point/>
+          </v-col>
+          <v-col cols="3">
+            <Inventory/>
+          </v-col>
+        </v-row>
+        <v-row no-gutters dense>
+          <v-col cols="5">
+            <Status/>
+          </v-col>
+          <v-col cols="3">
+            <Turn/>
+          </v-col>
+          <v-col cols="4">
+            <Armor/>
+          </v-col>
+        </v-row>
+        <v-row no-gutters>
+          <v-col cols="12">
+            <Footer/>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header'
+import Map from './components/Map'
+import Stamina from "@/components/Stamina";
+import Point from "@/components/Point";
+import Inventory from "@/components/Inventory";
+import Status from "@/components/Status";
+import Turn from "@/components/Turn";
+import Armor from "@/components/Armor";
+import Footer from "@/components/Footer";
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    Footer,
+    Armor,
+    Turn,
+    Status,
+    Inventory,
+    Header,
+    Map,
+    Stamina,
+    Point
+  },
+
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  .container{
+    max-width: 1600px;
+  }
 </style>
